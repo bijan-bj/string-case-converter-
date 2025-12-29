d8d59acca52b05b5b3fba55467f6fc1d843d1888
Bijan Jabbari <bijanjabbari.bjn85@gmail.com>
2025-12-29 20:13:52 +0330

    Add files via upload

---
+++ string uppercase_lowercase.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string word;
+	cout << "Enter a string :";
+	getline(cin, word);
+	for (int i = 0; i < word.size(); i++)
+	{
+		if (islower(word[i]))
+		{
+			word[i] = toupper(word[i]);
+		}
+		else
+		{
+			word[i] = tolower(word[i]);
+		}
+	}
+	cout << "Result: " << word << endl;
+}
